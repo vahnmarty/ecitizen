b70fe74ed569b64fd2f7eb46769af37fca371171
Vahn <vahnmarty@gmail.com>
2022-12-02 00:47:10 +0800

    fakers

--- app/Http/Livewire/Report/EmergencyDashboard.php
+++ app/Http/Livewire/Report/EmergencyDashboard.php
@@ -6,8 +6,34 @@ use Livewire\Component;
 
 class EmergencyDashboard extends Component
 {
+    public $alerts = [];
+
     public function render()
     {
         return view('livewire.report.emergency-dashboard')->layout('layouts.blank');
+    }
+
+    public function mount()
+    {
+        $this->getFakers();
+    }
+
+    public function getFakers()
+    {
+        $this->alerts[] = [
+            'type' => 'Fire',
+            'location' => 'Zone Meteor IISHAI Suarez',
+            'time' => now()->diffForHumans(),
+            'user' => 'Anonymous',
+            'description' => 'Naay sunog sa likod sa basketbolan kaning doul sa menteryo'
+        ];
+
+        $this->alerts[] = [
+            'type' => 'Flood',
+            'location' => 'Purok 10 Bagong Silang',
+            'time' => now()->diffForHumans(),
+            'user' => 'Anonymous',
+            'description' => 'Ga-baha dari sa may parkingan doul sa water station'
+        ];
     }
 }
--- resources/views/livewire/report/emergency-dashboard.blade.php
+++ resources/views/livewire/report/emergency-dashboard.blade.php
@@ -12,7 +12,7 @@
 
     <div class="fixed inset-y-0 pr-32 right-0 min-w-[16rem] bg-transparent py-12 max-h-screen overflow-auto">
         <div class="grid grid-cols-1 gap-6">
-            @foreach(range(1,5) as $rand)
+            @foreach($alerts as $alert)
             <div class="p-5 shadow-lg rounded-md  bg-white w-[28rem]">
                 <div class="flex gap-8">
                     <div class="space-y-1">
@@ -21,7 +21,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-green-600">
                                 <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
                               </svg>
-                            <p>Zone Meteor, IISHAI, Suarez</p>
+                            <p>{{ $alert['location'] }}</p>
                         </div>
                         <div class="flex w-full space-x-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -29,13 +29,13 @@
                               </svg>
                               
                               
-                            <p>5 seconds ago..</p>
+                            <p>{{ $alert['time'] }}</p>
                         </div>
                         <div class="flex w-full space-x-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                               </svg>
-                            <p>Anonymous</p>
+                            <p>{{ $alert['user'] }}</p>
                         </div>
                         <div class="flex space-x-2">
                             <div class="w-6 h-6 grow">
@@ -44,15 +44,23 @@
                                   </svg>
                             </div>
                             <div>
-                                <div class="italic text-sm break-all">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam autem ex vel earum, .</div>
+                                <div class="italic text-sm ">
+                                    {{ $alert['description'] }}
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <div class="w-14 h-14">
+                        @if($alert['type'] == 'Fire')
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-14 h-14 text-red-700 grow animate-pulse">
                             <path fill-rule="evenodd" d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z" clip-rule="evenodd" />
                         </svg>
+                        @else
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 text-blue-700 grow animate-pulse">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                          </svg>
+                        @endif
                     </div>
                 </div>
             </div>
